bd9ced10d44d8ce483355bce14249492156f8b80
MarwanTukhta <56633651+MarwanTukhta@users.noreply.github.com>
2022-01-26 17:02:57 +0300

    Update LibraryManager.rb

--- LibraryManager.rb
+++ LibraryManager.rb
@@ -2,80 +2,43 @@ require_relative 'Magazine.rb'
 require_relative 'Books.rb'
 
 class LibraryManager
-    def addMagazine(title, price, publisher_agent, date)
-        magazinee = Magazine.new(title,price,publisher_agent,date)
-        magazinee.addToDB
+    attr_accessor :magazines
+    
+    def initialize()
+        updateMagazines()
     end
 
-    def addBooks(title,price,author_name,number_of_pages,isbn)
-        books = Books.new(title,price,author_name,number_of_pages,isbn)
-        books.addToDB
+    def addMagazine(title, price, publisher_agent, date)
+        new_magazine = Magazine.new(title,price,publisher_agent,date)
+        new_magazine.addToDB
+        updateMagazines()
     end
+
     def getMagazinesByDate(date)
+        return @magazines.select {|e| e.date == date}
+    end
+    
+    def deleteMagazine(title)
+        magazine = @magazines.select {|e| e.title == title}
+        if magazine.length == 0
+            return "error"
+        else
+            magazine[0].delete
+            updateMagazines()
+        end
+    end
+
+    def updateMagazines()
         file = File.open("Magazine.txt")
         file_data = file.readlines.map(&:chomp)
-        magazines = []
+        @magazines = []
         file_data.each{|e|
             if e != ""
                 magazineArr = e.split(",")
-                magazine = Hash.new()
-                magazine["title"] = magazineArr[0]
-                magazine["price"] = magazineArr[1]
-                magazine["publisher-agent"] = magazineArr[2]
-                magazine["date"] = magazineArr[3]
-                magazines.push(magazine)
+                magazine = Magazine.new(magazineArr[0], magazineArr[1], magazineArr[2], magazineArr[3])
+                @magazines.push(magazine)
             end
         }
-        return magazines.select {|e| e["date"] == date}
+        file.close
     end
-
-    def range(from,to)
-        
-        file = File.open("Books.txt")
-        file_price = file.readlines.map(&:chomp)
-        books = []
-        file_price.each{|e|
-            if e != ""
-                booksArr = e.split(",")
-                books = Hash.new()
-                books["title"] = booksArr[0]
-                books["price"] = booksArr[1]
-                books["author_name"] = booksArr[2]
-                books["number_of_pages"] = booksArr[3]
-                books["isbn"] = booksArr[4]
-                books.push(books)
-            end
-        }
-        return books.select { e["price"].between?(from,to) }
-
-        def max(price)
-        
-            file = File.open("Books.txt")
-            file_price = file.readlines.map(&:chomp)
-            books = []
-            file_price.each{|e|
-                if e != ""
-                    booksArr = e.split(",")
-                    books = Hash.new()
-                    books["title"] = booksArr[0]
-                    books["price"] = booksArr[1]
-                    books["author_name"] = booksArr[2]
-                    books["number_of_pages"] = booksArr[3]
-                    books["isbn"] = booksArr[4]
-                    books.push(books)
-                end
-            }
-            return books.select { e["price"].max()}
-        end
-
-        def deleteBooks(title,price,author_name,number_of_pages,isbn)
-            books = Books.new(title,price,author_name,number_of_pages,isbn)
-            books.delete()
-        end
-
-        def listBooks(title,price,author_name,number_of_pages,isbn)
-            books = Books.new(title,price,author_name,number_of_pages,isbn)
-            books.to_s()
-        end
-
 end
