84b0da9f83893cf29daf84ae55c51a2b1cec746d
Bayader Alomari <bayaderalomari@Bayaders-MacBook-Pro.local>
2022-01-26 19:54:52 +0300

    update on Books.rb

--- Books.rb
+++ Books.rb
@@ -1,19 +1,34 @@
-require_relative 'Book_store.rb'
-
+require 'Book_store.rb'
+require 'fileutils'
 class Books < Storeitem
   attr_accessor :author_name,:number_of_pages,:isbn
 
-  def initialize(title,price,author_name,number_of_pages,isbn)
-      super(title,price)
-      @author_name = author_name
-      @number_of_pages = number_of_pages
-      @isbn = isbn    
+  def initilize(title,price,author_name,number_of_pages,isbn)
+    @title = title
+    @price = price
+    super(title,price)
+    @author_name = author_name
+    @number_of_pages = number_of_pages
+    @isbn = isbn  
   end
 
   def getbooks()
-       books = {1:{title: "Ruby Cookbook", price: "100.3", author_name: "Lucas Carlson", pages: "400", isbn: "9780596523695" },
-                2:{title: "Ruby programming", price: "90", author_name: "Jerry Lee", pages: "200", isbn: "1730497513499" },
-                3:{title: "Intro to Ruby", price: "80", author_name: "John Elder", pages: "550", isbn: "1284396855669"}
-      }
+    books = {1:{title: "Ruby Cookbook", price: "100", author_name: "Lucas Carlson", number_of_pages: "400", isbn: "9780596523695"},
+             2:{title: "Ruby programming", price: "90", author_name: "Jerry Lee", number_of_pages: "200", isbn: "1730497513499"},
+             3:{title: "Intro to Ruby", price: "80", author_name: "John Elder", number_of_pages: "550", isbn: "1284396855669"}
+   }
   end
+
+  def deleteBooks()
+    open('Books.txt', 'r') do |f|
+      open('Books.txt.tmp', 'w') do |f2|
+        f.each_line do |line|
+           f2.write(line) unless line.start_with? @title
+          end
+        end
+    end
+
+    FileUtils.mv 'Books.txt.tmp', 'Books.txt'
+  end
+
 end
