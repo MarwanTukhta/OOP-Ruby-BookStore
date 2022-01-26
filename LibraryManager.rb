a737f321be5006912a1a76c9f768a3075327f954
Bayader Alomari <bayaderalomari@Bayaders-MacBook-Pro.local>
2022-01-26 17:31:44 +0300

    update library manager with books methods

--- LibraryManager.rb
+++ LibraryManager.rb
@@ -3,9 +3,11 @@ require_relative 'Books.rb'
 
 class LibraryManager
     attr_accessor :magazines
+    attr_accessor :books
     
     def initialize()
         updateMagazines()
+        updateBooks()
     end
 
     def addMagazine(title, price, publisher_agent, date)
@@ -41,4 +43,46 @@ class LibraryManager
         }
         file.close
     end
-end
+
+    def addBooks(title, price, author_name, number_of_pages, isbn)
+        new_books = Books.new(title,price,publisher_agent,date)
+        new_books.addToDB
+        updateBooks()
+    end
+
+    def updateBooks()
+        file = File.open("Books.txt")
+        file_data = file.readlines.map(&:chomp)
+        @books = []
+        file_data.each{|e|
+            if e != ""
+                booksArr = e.split(",")
+                books = Books.new(booksArr[0], booksArr[1], booksArr[2], booksArr[3])
+                @books.push(books)
+            end
+        }
+        file.close
+    end
+    def deleteBook(title)
+        book = @book.select {|e| e.title == title}
+        if book.length == 0
+            return "error"
+        else
+            book[0].delete
+            updateBook()
+        end
+    end
+    
+    
+    def mostExpensiveBook(price)
+
+        return @books.select { e["price"].max()}
+    end
+
+    def range(from,to)
+            
+        return @books.select { e["price"].between?(from,to) }
+    end
+        
+     
+end 
